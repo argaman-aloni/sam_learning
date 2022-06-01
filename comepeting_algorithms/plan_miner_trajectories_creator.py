dbc96d3587811eab7412c910129dfe23a227db5a
Argaman Mordoch <argaman.aloni@gmail.com>
2022-06-01 22:03:14 +0300

    added the code that supports ENHSP solving algorithm.

--- comepeting_algorithms/plan_miner_trajectories_creator.py
+++ comepeting_algorithms/plan_miner_trajectories_creator.py
@@ -5,11 +5,16 @@ from pathlib import Path
 from typing import NoReturn
 
 from pddl_plus_parser.exporters import MetricFFParser, TrajectoryExporter
-from pddl_plus_parser.lisp_parsers import DomainParser, ProblemParser
+from pddl_plus_parser.lisp_parsers import DomainParser, ProblemParser, TrajectoryParser
+from pddl_plus_parser.models import Operator
 
 
 class PlanMinerTrajectoriesCreator:
-    """Class that transforms our trajectory file data into the type of trajectories that plan miner accepts."""
+    """Class that transforms our trajectory file data into the type of trajectories that plan miner accepts.
+
+    Note:
+        This assumes that there are regular trajectories to be converted to the correct format.
+    """
     domain_file_name: str
     working_directory_path: Path
     logger: logging.Logger
@@ -23,19 +28,35 @@ class PlanMinerTrajectoriesCreator:
         """Creates the domain trajectory files."""
         domain_file_path = self.working_directory_path / self.domain_file_name
         domain = DomainParser(domain_file_path).parse_domain()
+        pm_trajectories = []
         for trajectory_file_path in self.working_directory_path.glob("*.trajectory"):
+            plan_miner_trajectory_file_path = self.working_directory_path / f"{trajectory_file_path.stem}.pts"
+            if plan_miner_trajectory_file_path.exists():
+                continue
+
             problem_file_path = self.working_directory_path / f"{trajectory_file_path.stem}.pddl"
-            trajectory_file_path = self.working_directory_path / f"{trajectory_file_path.stem}.trajectory"
-            if trajectory_file_path.exists():
-                continue
+            problem = ProblemParser(problem_file_path, domain).parse_problem()
+            observation = TrajectoryParser(domain, problem).parse_trajectory(trajectory_file_path)
+            plan_sequence = ["##Tasks##"]
+            state_sequence = ["##States##"]
+            for index, action_triplet in enumerate(observation.components):
+                action = action_triplet.grounded_action_call
+                prev_state = action_triplet.previous_state
+                next_state = action_triplet.next_state
+                op = Operator(action=domain.actions[action.name], domain=domain, grounded_action_call=action.parameters)
+                plan_sequence.append(f"[{index}, {index + 1}]: {op.typed_action_call}")
+                if index == 0:
+                    state_sequence.append(f"[{index}]: {prev_state.typed_serialize()}")
 
-            problem = ProblemParser(problem_path=problem_file_path, domain=domain).parse_problem()
-            try:
-                triplets = trajectory_exporter.parse_plan(problem, trajectory_file_path)
-                trajectory_exporter.export_to_file(triplets, trajectory_file_path)
+                state_sequence.append(f"[{index + 1}]: {next_state.typed_serialize()}")
 
-            except (ValueError, IndexError):
-                continue
+            action_trace = "\n".join(plan_sequence)
+            state_trace = "\n".join(state_sequence)
+            plan_miner_trajectory = f"New plan!!!\n\n" \
+                                    f"{action_trace}\n\n" \
+                                    f"{state_trace}"
+
+            pm_trajectories.append(plan_miner_trajectory)
 
 
 if __name__ == '__main__':
--- solvers/enhsp_solver.py
+++ solvers/enhsp_solver.py
@@ -1,40 +1,17 @@
 """Module responsible for running the Expressive Numeric Heuristic Planner (ENHSP)."""
 import logging
-import os
-import re
 import subprocess
 import sys
-import time
 from pathlib import Path
-from typing import NoReturn
 
-execution_script = """#!/bin/bash
-
-################################################################################################
-### sbatch configuration parameters must start with #SBATCH and must precede any other commands.
-### To ignore, just add another # - like so: ##SBATCH
-################################################################################################
-
-#SBATCH --partition main			### specify partition name where to run a job. short: 7 days limit; gtx1080: 7 days; debug: 2 hours limit and 1 job at a time
-#SBATCH --time 0-03:30:00			### limit the time of job running. Make sure it is not greater than the partition time limit!! Format: D-H:MM:SS
-#SBATCH --job-name ehsp_planner_job			### name of the job
-#SBATCH --output job-%J.out			### output log for running job - %J for job number
-##SBATCH --mail-user=aaa.bbb@ccc	### user's email for sending job status messages
-##SBATCH --mail-type=END			### conditions for sending the email. ALL,BEGIN,END,FAIL, REQUEU, NONE
-
-##SBATCH --mem=32G				### amount of RAM memory
-##SBATCH --cpus-per-task=6			### number of CPU cores
-
-module load anaconda
-conda info --envs
-source activate pol_framework
-
-java -jar {planner_jar_path} -o {domain_file_path} -f {problem_file_path} -sp {plan_file_path} -anytime -timeout {timeout} > {report_file_path}
-"""
+from jdk4py import JAVA
+from typing import Dict
 
 ENHSP_FILE_PATH = "/sise/home/mordocha/numeric_planning/ENHSP/enhsp.jar"
-BATCH_JOB_SUBMISSION_REGEX = re.compile(b"Submitted batch job (?P<batch_id>\d+)")
 MAX_RUNNING_TIME = 60  # seconds
+
+TIMEOUT_ERROR_CODE = b"Timeout has been reached"
+PROBLEM_SOLVED = b"Problem Solved"
 
 
 class ENHSPSolver:
@@ -45,58 +22,49 @@ class ENHSPSolver:
     def __init__(self):
         self.logger = logging.getLogger(__name__)
 
-    def write_batch_and_execute_solver(self, script_file_path: Path, problems_directory_path: Path,
-                                       domain_file_path: Path, report_file_path: Path) -> NoReturn:
-        """Writes the batch file script to run on the cluster and then executes the solver algorithm.
+    def write_batch_and_execute_solver(self, problems_directory_path: Path, domain_file_path: Path) -> Dict[str, str]:
+        """Solves numeric and PDDL+ problems using the ENHSP algorithm, automatically outputs the solution into a file.
 
-        :param script_file_path: the path to output the learning script.
-        :param problems_directory_path: the directory containing the problems that are needed to be solved.
-        :param domain_file_path: the path to the domain file that will be used as an input to the planner.
-        :param report_file_path: the path to the report file containing the STDOUT.
-            Used to understand the outcome of the planner's execution.
+        :param problems_directory_path: the path to the problems directory.
+        :param domain_file_path: the path to the domain file.
+        :return: dictionary mapping the problem file name to its solution status.
         """
+        solving_stats = {}
         self.logger.info("Starting to solve the input problems using ENHSP solver.")
         for problem_file_path in problems_directory_path.glob("pfile*.pddl"):
             self.logger.debug(f"Starting to work on solving problem - {problem_file_path.stem}")
             solution_path = problems_directory_path / f"{problem_file_path.stem}.solution"
-            completed_file_str = execution_script.format(
-                planner_jar_path=ENHSP_FILE_PATH,
-                domain_file_path=str(domain_file_path.absolute()),
-                problem_file_path=str(problem_file_path.absolute()),
-                plan_file_path=str(solution_path.absolute()),
-                timeout=MAX_RUNNING_TIME,
-                report_file_path=report_file_path
-            )
+            running_options = ["-o", str(domain_file_path.absolute()),
+                               "-f", str(problem_file_path.absolute()),
+                               "-planner", "sat-hmrphj",
+                               "-sp", str(solution_path.absolute())]
+            try:
+                process = subprocess.run([str(JAVA), "-jar", ENHSP_FILE_PATH, *running_options], capture_output=True,
+                                         timeout=MAX_RUNNING_TIME + 1)
+                self.logger.info("ENHSP finished its execution!")
+                if PROBLEM_SOLVED in process.stdout:
+                    self.logger.debug(f"Solver succeeded in solving problem - {problem_file_path.stem}")
+                    solving_stats[problem_file_path.stem] = "ok"
 
-            with open(script_file_path, "wt") as run_script_file:
-                run_script_file.write(completed_file_str)
+                else:
+                    self.logger.debug(f"Solver could not solve problem - {problem_file_path.stem}")
+                    solving_stats[problem_file_path.stem] = "no_solution"
 
-            subprocess.check_output(["sbatch", str(script_file_path)])
-            self.logger.debug("Waiting for the batch script to start running.")
-            time.sleep()
-            start_time = time.time()
-            while time.time() - start_time < MAX_RUNNING_TIME:
-                time.sleep(1)
+            except subprocess.TimeoutExpired:
+                self.logger.debug(f"Learning algorithm did not finish in time so was killed "
+                                  f"while trying to solve - {problem_file_path.stem}")
+                solving_stats[problem_file_path.stem] = "timeout"
+                continue
 
-            self.logger.info("ENHSP finished its execution!")
-            self.logger.debug("Cleaning the sbatch file from the problems directory.")
-            os.remove(script_file_path)
-            for job_file_path in Path(os.getcwd()).glob("job-*.out"):
-                self.logger.debug("Removing the temp job file!")
-                try:
-                    os.remove(job_file_path)
-
-                except FileNotFoundError:
-                    continue
-
-        return
+        return solving_stats
 
 
 if __name__ == '__main__':
     args = sys.argv
-    logging.basicConfig(level=logging.DEBUG)
+    logging.basicConfig(
+        format="%(asctime)s %(levelname)-8s %(message)s",
+        datefmt="%Y-%m-%d %H:%M:%S",
+        level=logging.DEBUG)
     solver = ENHSPSolver()
-    solver.write_batch_and_execute_solver(script_file_path=Path(args[1]),
-                                          problems_directory_path=Path(args[2]),
-                                          domain_file_path=Path(args[3]),
-                                          report_file_path=Path(args[4]))
+    solver.write_batch_and_execute_solver(problems_directory_path=Path(args[1]),
+                                          domain_file_path=Path(args[2]))
