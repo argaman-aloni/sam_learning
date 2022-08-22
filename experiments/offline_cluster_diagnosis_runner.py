e7da69065e8412b09c8b820e1e90b47fd83c7ea6
Argaman Mordoch <argaman.aloni@gmail.com>
2022-08-22 11:41:28 +0300

    fixed minor things that prevented from the code to run offline.

--- experiments/offline_cluster_diagnosis_runner.py
+++ experiments/offline_cluster_diagnosis_runner.py
@@ -6,4 +6,4 @@ if __name__ == '__main__':
     args = sys.argv
     os.system(f"nohup bash -c '{sys.executable} model_fault_diagnosis.py "
               f"--work_dir_path {args[1]} --fluents_map_path {args[2]} "
-              f"--original_domain_file_name {args[3]}' &")
+              f"--original_domain_file_name {args[3]} > /dev/null ' &")
