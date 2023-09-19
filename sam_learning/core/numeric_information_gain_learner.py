made some changes to the calculation of the convex hull and fixed some parts relating to the PCA and starting to optimize the online code.

sam_learning/core/numeric_information_gain_learner.py
@@ -1,12 +1,13 @@
 """A module containing the algorithm to calculate the information gain of new samples."""
 import logging
-from typing import Dict, List
+from typing import Dict, List, Tuple
 
 import numpy as np
 from matplotlib import pyplot as plt
 from pandas import DataFrame, Series
 from pddl_plus_parser.models import PDDLFunction, Predicate
 from scipy.spatial import Delaunay, QhullError, delaunay_plot_2d
+from sklearn.decomposition import PCA
 
 from sam_learning.core.numeric_utils import get_num_independent_equations, filter_constant_features, \
     detect_linear_dependent_features
@@ -22,6 +23,8 @@ class InformationGainLearner:
     positive_samples_df: DataFrame
     negative_samples_df: DataFrame
     _effects_learned_perfectly: bool
+    _cached_convex_hull: Delaunay
+    _pca_model: PCA
 
     def __init__(self, action_name: str, lifted_functions: List[str], lifted_predicates: List[str]):
         self.logger = logging.getLogger(__name__)
@@ -32,6 +35,7 @@ class InformationGainLearner:
         self.negative_samples_df = DataFrame(columns=lifted_functions + lifted_predicates)
         self._effects_learned_perfectly = False
         self._cached_convex_hull = None
+        self._pca_model = None
 
     def _locate_sample_in_df(self, sample_to_locate: List[float], df: DataFrame) -> int:
         """Locates the sample in the data frame.
@@ -97,6 +101,45 @@ class InformationGainLearner:
 
         return True
 
+    def _calculate_whether_in_delanauy_hull(
+            self, convex_hull_points: np.ndarray,
+            new_point: np.ndarray, debug_mode: bool = False, use_cached_ch: bool = False, ) -> bool:
+        """
+
+        :param convex_hull_points:
+        :param new_point:
+        :param debug_mode:
+        :param use_cached_ch:
+        :return:
+        """
+        if self._cached_convex_hull is not None and use_cached_ch:
+            delaunay_hull = self._cached_convex_hull
+            relevant_sample = new_point if not self._pca_model is None else self._pca_model.transform(new_point)
+
+        else:
+            try:
+                delaunay_hull = Delaunay(convex_hull_points)
+                self._pca_model = None
+                relevant_sample = new_point
+                if use_cached_ch:
+                    self._cached_convex_hull = delaunay_hull
+
+            except (QhullError, ValueError):
+                rank = np.linalg.matrix_rank(convex_hull_points)
+                model = PCA(n_components=rank - 1).fit(convex_hull_points)
+                pred_points = model.transform(convex_hull_points)
+                delaunay_hull = Delaunay(pred_points)
+                relevant_sample = model.transform(new_point)
+
+        if debug_mode:
+            self._display_delaunay_graph(delaunay_hull, convex_hull_points.shape[1])
+
+        result = delaunay_hull.find_simplex(relevant_sample) >= 0
+        if isinstance(result, np.bool_):
+            return result
+
+        return any(result)
+
     def _in_hull(
             self, points_to_test: DataFrame, hull_df: DataFrame, debug_mode: bool = False,
             use_cached_ch: bool = False) -> bool:
@@ -137,20 +180,7 @@ class InformationGainLearner:
         if hull.shape[1] == 1:
             return all([hull.min() <= point <= hull.max() for point in relevant_sample.to_numpy()])
 
-        if use_cached_ch:
-            delaunay_hull = self._cached_convex_hull if self._cached_convex_hull is not None else Delaunay(hull)
-
-        else:
-            delaunay_hull = Delaunay(hull)
-
-        if debug_mode:
-            self._display_delaunay_graph(delaunay_hull, hull.shape[1])
-
-        result = delaunay_hull.find_simplex(relevant_sample.to_numpy()) >= 0
-        if isinstance(result, np.bool_):
-            return result
-
-        return any(result)
+        return self._calculate_whether_in_delanauy_hull(hull, relevant_sample.to_numpy(), debug_mode, use_cached_ch)
 
     def _remove_features(self, features_to_keep: List[str], features_list: List[str]) -> List[str]:
         """Removes features from the data frames.

tests/numeric_utils_test.py
@@ -1,7 +1,9 @@
 """Tests for numeric_utils.py"""
+import numpy as np
 import pandas as pd
+from sklearn.decomposition import PCA
 
-from sam_learning.core.numeric_utils import extract_numeric_linear_coefficient
+from sam_learning.core.numeric_utils import extract_numeric_linear_coefficient, construct_pca_variable_strings
 
 
 def test_extract_numeric_linear_coefficient_does_not_output_infinity_for_zero_division():
@@ -17,3 +19,24 @@ def test_extract_numeric_linear_coefficient_does_not_output_nan_for_illegal_divi
 def test_extract_numeric_linear_coefficient_outputs_correct_coefficient_when_division_is_legal():
     """Test that the function outputs the correct coefficient when the division is legal."""
     assert extract_numeric_linear_coefficient(pd.Series([1, 2, 3]), pd.Series([1, 2, 3])) == 1
+
+
+def test_construct_pca_variable_strings_returns_correct_strings_representing_the_pca_format():
+    function_variables = ["(x)", "(y)", "(z)"]
+    X = np.array([[-1, -1, 1], [-2, -1, 3], [-3, -2, 23], [1, 1, 31], [2, 1, 12], [3, 2, 7]])
+    pca = PCA(n_components=2)
+    pca.fit(X)
+
+    pca_variable_strings = construct_pca_variable_strings(function_variables, pca.mean_, pca.components_)
+    assert len(pca_variable_strings) == 2  # 2 components
+
+
+def test_construct_pca_variable_strings_returns_correct_strings_representing_the_pca_format_when_components_are_equal_to_variables():
+    function_variables = ["(x)", "(y)", "(z)"]
+    X = np.array([[-1, -1, 1], [-2, -1, 3], [-3, -2, 23], [1, 1, 31], [2, 1, 12], [3, 2, 7]])
+    pca = PCA(n_components=3)
+    pca.fit(X)
+
+    pca_variable_strings = construct_pca_variable_strings(function_variables, pca.mean_, pca.components_)
+    assert len(pca_variable_strings) == 3  # 3 components
+    print(pca_variable_strings)