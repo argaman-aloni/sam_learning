00e2a0b01d9a70a860a15f2f059022fc0a659447
Argaman Mordoch <argaman.aloni@gmail.com>
2024-07-08 17:06:07 +0300

    committed a bug fix and combined the results collection and the plotting

--- experiments/cluster_scripts/numeric_experiments_sbatch_runner.py
+++ experiments/cluster_scripts/numeric_experiments_sbatch_runner.py
@@ -119,7 +119,7 @@ def submit_job_and_validate_execution(code_directory, configurations, experiment
     dependency_argument = None if not fold_creation_sid else f"afterok:{fold_creation_sid}"
     sid = submit_job(
         conda_env="online_nsam",
-        mem="8G",
+        mem="16G",
         python_file=f"{code_directory}/{configurations['experiments_script_path']}",
         jobname=job_name,
         dependency=dependency_argument,
