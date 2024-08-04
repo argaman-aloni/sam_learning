5d6574ef103a8633f3ceac2ba3da275a801116fb
Argaman Mordoch <argaman.aloni@gmail.com>
2024-08-04 16:55:51 +0300

    Removed the side effect of the solver and now this is a parameter to the solving function.
    This change should considerably lower the time it takes to solve problems during experimentation since if a problem is solved it is tried again. Furthermore, this will output more accurate statistics regarading the solving rate.

--- experiments/conditional_effects_experiment_runner.py
+++ experiments/conditional_effects_experiment_runner.py
@@ -17,11 +17,21 @@ from validators import DomainValidator
 class OfflineConditionalEffectsExperimentRunner(OfflineBasicExperimentRunner):
     """Class to conduct offline numeric action model learning experiments."""
 
-    def __init__(self, working_directory_path: Path, domain_file_name: str, universals_map_path: Optional[Path],
-                 max_num_antecedents: int, problem_prefix: str = "pfile"):
-        super().__init__(working_directory_path=working_directory_path, domain_file_name=domain_file_name,
-                         learning_algorithm=LearningAlgorithmType.universal_sam, solver_type=SolverType.metric_ff,
-                         problem_prefix=problem_prefix)
+    def __init__(
+        self,
+        working_directory_path: Path,
+        domain_file_name: str,
+        universals_map_path: Optional[Path],
+        max_num_antecedents: int,
+        problem_prefix: str = "pfile",
+    ):
+        super().__init__(
+            working_directory_path=working_directory_path,
+            domain_file_name=domain_file_name,
+            learning_algorithm=LearningAlgorithmType.universal_sam,
+            solver_type=SolverType.metric_ff,
+            problem_prefix=problem_prefix,
+        )
         self.max_num_antecedents = max_num_antecedents
         if universals_map_path is not None:
             with open(universals_map_path, "rt") as json_file:
@@ -32,13 +42,15 @@ class OfflineConditionalEffectsExperimentRunner(OfflineBasicExperimentRunner):
 
         self.semantic_performance_calc = None
         self.domain_validator = DomainValidator(
-            self.working_directory_path, LearningAlgorithmType.universal_sam,
+            self.working_directory_path,
+            LearningAlgorithmType.universal_sam,
             self.working_directory_path / domain_file_name,
-            solver_type=SolverType.fast_forward, problem_prefix=problem_prefix)
+            problem_prefix=problem_prefix,
+        )
 
     def _apply_learning_algorithm(
-            self, partial_domain: Domain, allowed_observations: List[Observation],
-            test_set_dir_path: Path) -> Tuple[LearnerDomain, Dict[str, str]]:
+        self, partial_domain: Domain, allowed_observations: List[Observation], test_set_dir_path: Path
+    ) -> Tuple[LearnerDomain, Dict[str, str]]:
         """Learns the action model using the numeric action model learning algorithms.
 
         :param partial_domain: the partial domain without the actions' preconditions and effects.
@@ -47,9 +59,9 @@ class OfflineConditionalEffectsExperimentRunner(OfflineBasicExperimentRunner):
         :return: the learned action model and the learned action model's learning statistics.
         """
 
-        learner = UniversallyConditionalSAM(partial_domain=partial_domain,
-                                            max_antecedents_size=self.max_num_antecedents,
-                                            universals_map=self.action_to_universals_map)
+        learner = UniversallyConditionalSAM(
+            partial_domain=partial_domain, max_antecedents_size=self.max_num_antecedents, universals_map=self.action_to_universals_map
+        )
         return learner.learn_action_model(allowed_observations)
 
     def run_fold(self, fold_num: int, train_set_dir_path: Path, test_set_dir_path: Path) -> None:
@@ -65,20 +77,20 @@ class OfflineConditionalEffectsExperimentRunner(OfflineBasicExperimentRunner):
 
 
 def parse_arguments() -> argparse.Namespace:
-    parser = argparse.ArgumentParser(
-        description="Runs the numeric action model learning algorithms evaluation experiments.")
-    parser.add_argument("--working_directory_path", required=True,
-                        help="The path to the directory where the domain is")
+    parser = argparse.ArgumentParser(description="Runs the numeric action model learning algorithms evaluation experiments.")
+    parser.add_argument("--working_directory_path", required=True, help="The path to the directory where the domain is")
     parser.add_argument("--domain_file_name", required=True, help="the domain file name including the extension")
-    parser.add_argument("--universals_map", required=False,
-                        help="The path to the file mapping indicating whether there are universals or not",
-                        default=None)
-    parser.add_argument("--learning_algorithm", required=True, type=int, help="The type of learning algorithm. "
-                             "\n3: numeric_sam\n4: raw_numeric_sam\n 6: polynomial_sam\n ")
-    parser.add_argument("--max_antecedent_size", required=False, type=int, help="The maximum antecedent size",
-                        default=1)
-    parser.add_argument("--problems_prefix", required=False, help="The prefix of the problems' file names",
-                        type=str, default="pfile")
+    parser.add_argument(
+        "--universals_map", required=False, help="The path to the file mapping indicating whether there are universals or not", default=None
+    )
+    parser.add_argument(
+        "--learning_algorithm",
+        required=True,
+        type=int,
+        help="The type of learning algorithm. " "\n3: numeric_sam\n4: raw_numeric_sam\n 6: polynomial_sam\n ",
+    )
+    parser.add_argument("--max_antecedent_size", required=False, type=int, help="The maximum antecedent size", default=1)
+    parser.add_argument("--problems_prefix", required=False, help="The prefix of the problems' file names", type=str, default="pfile")
     parser.add_argument("--fold_number", required=True, help="The number of the fold to run", type=int)
     args = parser.parse_args()
     return args
@@ -93,12 +105,14 @@ def main():
         domain_file_name=args.domain_file_name,
         universals_map_path=Path(args.universals_map) if args.universals_map else None,
         max_num_antecedents=args.max_antecedent_size or 0,
-        problem_prefix=args.problems_prefix)
+        problem_prefix=args.problems_prefix,
+    )
     offline_learner.run_fold(
         fold_num=args.fold_number,
         train_set_dir_path=(working_directory_path / "train") / f"fold_{args.fold_number}_{args.learning_algorithm}",
-        test_set_dir_path=(working_directory_path / "test") / f"fold_{args.fold_number}_{args.learning_algorithm}")
+        test_set_dir_path=(working_directory_path / "test") / f"fold_{args.fold_number}_{args.learning_algorithm}",
+    )
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
