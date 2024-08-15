d8d73f394056f75f7277ee5cbf7630cff2ad5847
oriKay <karato@post.bgu.ac.il>
2024-08-15 12:48:43 +0300

    continuing

--- experiments/offline_remote_execution/offline_ma_cluster_experiment_runner.py
+++ experiments/offline_remote_execution/offline_ma_cluster_experiment_runner.py
@@ -10,9 +10,9 @@ if __name__ == '__main__':
     #           f"> results-{args[2]}.txt ' &")
 
     configurations = [
-        # ("/sise/home/karato/work_tools/DomainsData/satellite_enhanced/", "satellite_combined_domain.pddl",
-        #  "[satellite0,satellite1,satellite2,satellite3,satellite4,satellite5,satellite6,satellite7,satellite8,satellite9]",
-        #  "pfile", 1),
+        ("/sise/home/karato/work_tools/DomainsData/satellite_enhanced/", "satellite_combined_domain.pddl",
+         "[satellite0,satellite1,satellite2,satellite3,satellite4,satellite5,satellite6,satellite7,satellite8,satellite9]",
+         "pfile", 2),
         # ("/sise/home/karato/work_tools/DomainsData/blocksworld_enhanced/", "blocks_combined_domain.pddl",
         #  "[a1,a2,a3,a4]", "pfile", 1),
         # ("/sise/home/karato/work_tools/DomainsData/depots_enhanced/", "depots_combined_domain.pddl",
@@ -20,10 +20,10 @@ if __name__ == '__main__':
         #  "pfile", 1),
         # ("/sise/home/karato/work_tools/DomainsData/driverlog_enhanced/", "driverlog_combined_domain.pddl",
         #  "[driver1,driver2,driver3,driver4,driver5,driver6,driver7,driver8]", "pfile", 1),
-        ("/sise/home/karato/work_tools/DomainsData/logistics_enhanced/", "logistics_combined_domain.pddl",
-         "[apn1,apn2,tru1,tru2,tru3,tru4,tru5]", "pfile", 1),
-        ("/sise/home/karato/work_tools/DomainsData/rovers_enhanced/", "rover_combined_domain.pddl",
-         "[rover0,rover1,rover2,rover3,rover4,rover5,rover6,rover7,rover8,rover9]", "pfile", 1),
+        # ("/sise/home/karato/work_tools/DomainsData/logistics_enhanced/", "logistics_combined_domain.pddl",
+        #  "[apn1,apn2,tru1,tru2,tru3,tru4,tru5]", "pfile", 1),
+        # ("/sise/home/karato/work_tools/DomainsData/rovers_enhanced/", "rover_combined_domain.pddl",
+        #  "[rover0,rover1,rover2,rover3,rover4,rover5,rover6,rover7,rover8,rover9]", "pfile", 1),
         # ("/sise/home/karato/work_tools/DomainsData/sokoban_enhanced/", "sokoban_combined_domain.pddl",
         #  "[player-01,player-02,player-03,player-04]", "pfile")
     ]
