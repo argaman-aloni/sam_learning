247b6a97bdbcbcd5170b37b5cf1bbd3f707b4e0a
Argaman Mordoch <argaman.aloni@gmail.com>
2024-08-28 15:37:48 +0300

    fixed a bug that affected the measured results of minecraft domain.

--- statistics/utils.py
+++ statistics/utils.py
@@ -1,15 +1,13 @@
 """Utility functionality for the experiments"""
-import random
 from pathlib import Path
 from typing import List, Union
 from typing import Optional
 
 from pddl_plus_parser.lisp_parsers import DomainParser
 from pddl_plus_parser.lisp_parsers import ProblemParser, TrajectoryParser
-from pddl_plus_parser.models import Observation
 
-from statistics.semantic_performance_calculator import SemanticPerformanceCalculator
 from statistics.numeric_performance_calculator import NumericPerformanceCalculator
+from statistics.semantic_performance_calculator import SemanticPerformanceCalculator
 from utilities import LearningAlgorithmType
 
 DEFAULT_SIZE = 10
@@ -22,6 +20,7 @@ def init_semantic_performance_calculator(
     executing_agents: Optional[List[str]] = None,
     test_set_dir_path: Path = None,
     is_numeric: bool = False,
+    problem_prefix: str = "pfile",
 ) -> Union[NumericPerformanceCalculator, SemanticPerformanceCalculator]:
     """Initializes a numeric performance calculator object.
 
@@ -31,12 +30,13 @@ def init_semantic_performance_calculator(
     :param executing_agents: the agents that are executing the domain.
     :param test_set_dir_path: the path to the directory containing the test set.
     :param is_numeric: whether the performance calculator is numeric or not.
+    :param problem_prefix: the prefix of the problem files.
     :return: the initialized numeric performance calculator object.
     """
     domain_path = working_directory_path / domain_file_name
     model_domain = partial_domain = DomainParser(domain_path=domain_path, partial_parsing=False).parse_domain()
     observations = []
-    problem_files = list(test_set_dir_path.glob("pfile*.pddl"))
+    problem_files = list(test_set_dir_path.glob(f"{problem_prefix}*.pddl"))
     for test_problem_path in problem_files:
         trajectory_file_path = test_set_dir_path / f"{test_problem_path.stem}.trajectory"
         problem = ProblemParser(test_problem_path, partial_domain).parse_problem()
