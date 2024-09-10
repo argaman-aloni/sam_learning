48dbafc272189d360ea0adac0387c46759c3dc7d
oriKay <karato@post.bgu.ac.il>
2024-09-10 17:48:01 +0300

    working on the concurrent execution for ma sam experiments

--- experiments/concurrent_execution/parallel_ma_sam_experiment_runner.py
+++ experiments/concurrent_execution/parallel_ma_sam_experiment_runner.py
@@ -50,25 +50,28 @@ class SingleIterationMASAMExperimentRunner(ParallelExperimentRunner):
         )
         self.executing_agents = executing_agents
 
-
-    def _filter_baseline_single_agent_trajectory(self, complete_observation) -> Observation:
+    def _filter_baseline_single_agent_trajectory(self, complete_observations) -> List[Observation]:
         """Create a single agent observation from a multi-agent observation.
 
         :param complete_observation: the multi-agent observation to filter.
         :return: the filtered single agent observation.
         """
-        filtered_observation = Observation()
-        filtered_observation.add_problem_objects(complete_observation.grounded_objects)
-        for component in complete_observation.components:
-            if component.grounded_joint_action.action_count > 1:
-                self.logger.debug(f"Skipping the joint action - {component.grounded_joint_action} "
-                                  f"since it contains multiple agents executing at once.!")
-                continue
+        filtered_observations = []
+        for complete_observation in complete_observations:
+            filtered_observation = Observation()
+            filtered_observation.add_problem_objects(complete_observation.grounded_objects)
+            for component in complete_observation.components:
+                if component.grounded_joint_action.action_count > 1:
+                    self.logger.debug(f"Skipping the joint action - {component.grounded_joint_action} "
+                                      f"since it contains multiple agents executing at once.!")
+                    continue
 
-            filtered_observation.add_component(component.previous_state,
-                                               component.grounded_joint_action.operational_actions[0],
-                                               component.next_state)
-        return filtered_observation
+                filtered_observation.add_component(component.previous_state,
+                                                   component.grounded_joint_action.operational_actions[0],
+                                                   component.next_state)
+            filtered_observations.append(filtered_observation)
+
+        return filtered_observations
 
     def _apply_learning_algorithm(
         self, partial_domain: Domain, allowed_observations: List[Observation], test_set_dir_path: Path
