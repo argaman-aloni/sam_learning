fbf1c8cbb974e17a21e6045b0a9dfd89ca9a6347
Argaman Mordoch <argaman.aloni@gmail.com>
2024-09-25 19:15:48 +0300

    Added concurrent execution of fast downward

--- solvers/fast_downward_solver.py
+++ solvers/fast_downward_solver.py
@@ -114,4 +114,10 @@ if __name__ == "__main__":
     args = sys.argv
     logging.basicConfig(format="%(asctime)s %(levelname)-8s %(message)s", datefmt="%Y-%m-%d %H:%M:%S", level=logging.DEBUG)
     solver = FastDownwardSolver()
-    solver.execute_solver(problems_directory_path=Path(args[1]), domain_file_path=Path(args[2]))
+    solver.solve_problem(
+        domain_file_path=Path(args[1]),
+        problem_file_path=Path(args[2]),
+        problems_directory_path=Path(args[3]),
+        solving_stats={},
+        solving_timeout=int(args[4]),
+    )
