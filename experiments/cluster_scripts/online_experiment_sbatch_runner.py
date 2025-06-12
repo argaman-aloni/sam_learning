d9be2d729353cba870afcf8c9868bd1ad684b02a
Argaman Mordoch <argaman.aloni@gmail.com>
2025-06-12 12:21:23 +0300

    fixed (?) error in the cluster

--- experiments/cluster_scripts/online_experiment_sbatch_runner.py
+++ experiments/cluster_scripts/online_experiment_sbatch_runner.py
@@ -11,55 +11,10 @@ from experiments.cluster_scripts.common import (
     EXPERIMENTS_CONFIG_STR,
     submit_job_and_validate_execution,
     create_execution_arguments,
-    setup_experiments_folds_job,
-    validate_job_running,
+    create_all_experiments_folders,
 )
 
 signal.signal(signal.SIGINT, sigint_handler)
-
-
-def create_experiment_folders(
-    code_directory,
-    environment_variables,
-    experiment,
-):
-    print(f"Creating the directories containing the folds datasets for the experiments.")
-    sid = setup_experiments_folds_job(
-        code_directory=code_directory,
-        environment_variables=environment_variables,
-        experiment=experiment,
-        internal_iterations=None,
-        experiment_size=100,
-        should_create_random_trajectories=True,
-    )
-    return sid
-
-
-def create_all_experiments_folders(
-    code_directory,
-    environment_variables,
-    configurations,
-):
-    print("Creating the directories containing the folds datasets for the experiments.")
-    jobs_sids = []
-    output_internal_iterations = []
-    for experiment_index, experiment in enumerate(configurations[EXPERIMENTS_CONFIG_STR]):
-        fold_creation_sid = create_experiment_folders(
-            code_directory,
-            environment_variables,
-            experiment,
-        )
-        jobs_sids.append(fold_creation_sid)
-        print(
-            f"Submitted fold datasets folder creation job with the id {fold_creation_sid} for the experiment with domain {experiment['domain_file_name']}\n"
-        )
-
-    while any([validate_job_running(sid) is not None for sid in jobs_sids]):
-        print("Waiting for the fold creation jobs to finish.")
-        time.sleep(5)
-
-    print("Finished building the experiment folds!")
-    return output_internal_iterations
 
 
 def main():
@@ -67,7 +22,13 @@ def main():
     environment_variables = get_environment_variables(configurations)
     code_directory = configurations["code_directory_path"]
     print("Starting to setup and run the experiments!")
-    create_all_experiments_folders(code_directory, environment_variables, configurations)
+    create_all_experiments_folders(
+        code_directory,
+        environment_variables,
+        configurations,
+        should_create_random_trajectories=True,
+        should_create_internal_iterations=False,
+    )
     for experiment_index, experiment in enumerate(configurations[EXPERIMENTS_CONFIG_STR]):
         experiment_sids = []
         for fold in range(configurations["num_folds"]):
